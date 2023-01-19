22a487cd892a54bd25d750ee8a43b02823219e71
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-01-19 10:30:57 -0300

    Downgrade React v18 to v17

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
-        <Link to="/create">Create Recipe</Link>
+        <Link to="/create">Create recipe</Link>
       </nav>
     </div>
   );
