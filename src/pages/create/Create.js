defda857b95837dd3a0056d677625e7572254546
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-01-23 15:14:56 -0300

    Add search bar and search functionality

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 
 //styles
@@ -11,11 +12,18 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+  const history = useHistory();
 
   const { postData, data, error } = useFetch(
     "http://localhost:3000/recipes",
     "POST"
   );
+
+  useEffect(() => {
+    if (data) {
+      history.push("/");
+    }
+  }, [data]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,27 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
+//styles
 import "./Search.css";
 
+//components
+import RecipeList from "../../components/RecipeList";
+
 export default function Search() {
-  return <div>Search page</div>;
+  const querySearch = useLocation().search;
+  const queryParams = new URLSearchParams(querySearch);
+  const query = queryParams.get("q");
+
+  const url = "http://localhost:3000/recipes?q=" + query;
+
+  const { isPending, error, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Results for "{query}"</h2>
+      {isPending && <p className="loading">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
